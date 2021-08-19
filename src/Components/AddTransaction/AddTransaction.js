46663834f8eff297aca980880c28723fe7d2dc9e
Yaser <mohammadyaser1379@gmail.com>
2021-08-20 00:27:39 +0430

    AddTransaction component completed

--- src/Components/AddTransaction/AddTransaction.js
+++ src/Components/AddTransaction/AddTransaction.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const AddTransaction = () => {
+    const [title , setTitle] = useState("");
+    const [amount , setAmount] = useState(0);
+    const [type , setType] = useState(null);
+
+    const titleChangeHandler = (event)=>{
+        setTitle(event.target.value);
+    }
+    const amountChangeHandler=(event)=>{
+        setAmount(event.target.value);
+    }
+    const typeChangeHandler=(event)=>{
+        setType(event.target.value);
+    }
+    const submitHandler=(event)=>{
+        event.preventDefault();
+        if (title === "" || amount=== 0){
+            alert("You have to set transaction title and amount");
+        }
+        if (type === null){
+            alert("you have to set transaction type");
+        }
+    }
     return (
         <div>
-            add transaction component
+            <form onSubmit={submitHandler}>
+                <input
+                    type="text"
+                    placeholder={"Transaction Title (Required)"}
+                    onChange={titleChangeHandler}
+                    value={title}
+                />
+                <input
+                    type="number"
+                    placeholder={"Transaction Amount (Required)"}
+                    onChange={amountChangeHandler}
+                />
+                <div>
+                    <input onChange={typeChangeHandler} type="radio" name={"type"} value={"income"} id={"income"}/>
+                    <label htmlFor={"income"}>Income</label>
+                    <input onChange={typeChangeHandler} type="radio" name={"type"} value={"expense"} id={"expense"}/>
+                    <label htmlFor={"expense"}>Expense</label>
+                </div>
+                <button type={"submit"}>ADD</button>
+            </form>
         </div>
     );
 };
